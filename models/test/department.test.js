3b756b7ab1d5b00fb9c57e7043fbedd56a68ee7a
PawelJadach <pawel.jadach.96@gmail.com>
2020-03-24 13:20:18 +0100

    add test for employee and department models

---
+++ models/test/department.test.js
@@ -0,0 +1,64 @@
+const Department = require('../department.model.js');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Department', () => {
+
+  it('should throw an error if no "name" arg', () => {
+    const dep = new Department({}); 
+    dep.validate(err => {
+      expect(err.errors.name).to.exist;
+    });
+
+    after(() => {
+      mongoose.models = {};
+    });
+  });
+
+  it('should throw an error if "name" is not a string', () => {
+
+    const cases = [{}, []];
+    for(let name of cases) {
+      const dep = new Department({ name });
+
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+
+    after(() => {
+      mongoose.models = {};
+    });
+  });
+
+  it('should throw an error if "name" arg length is <5 or >20', () => {
+    const cases = [{name: 'asd'}, {name: 'asdasdasdasdasdafdadgsfvvccreagsdfagagdfdsfasdg'}];
+    for(let name of cases) {
+      const dep = new Department({ name });
+
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+
+    after(() => {
+      mongoose.models = {};
+    });
+  });
+
+  it('shouldnt throw an error if "name" arg length is OK', () => {
+    const cases = [{name: 'asdsafasf'}, {name: 'qcwdsfasfqwfasf'}];
+    for(let name of cases) {
+      const dep = new Department({ name });
+
+      dep.validate(err => {
+        expect(!err.errors.name).to.exist;
+      });
+    }
+
+    after(() => {
+      mongoose.models = {};
+    });
+  });
+ 
+});
---
+++ models/test/employee.test.js
@@ -0,0 +1,98 @@
+const Employee = require('../employees.model.js');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+
+describe('Employee', () => {
+
+  it('should throw an error if no have department arg or its not string', () => {
+
+      const tab = [
+        {
+          firstName: 'sad',
+          lastName: 'sad',
+        },
+        {
+          firstName: 'sad',
+          lastName: 'sad',
+          department: {},
+        },
+        {
+          firstName: 'sad',
+          lastName: 'sad',
+          department: [],
+        }
+      ]
+      
+      tab.map(option => {
+        const employee = new Employee({...option});
+        employee.validate(err => {
+          expect(err.errors.department).to.exist;
+        });
+      })
+      
+    after(() => {
+      mongoose.models = {};
+    });
+  });
+
+  it('should throw an error if no have firstname arg or its not string', () => {
+    const tab = [
+      {
+        department: 'sad',
+        lastName: 'sad',
+      },
+      {
+        department: 'sad',
+        lastName: 'sad',
+        firstName: {},
+      },
+      {
+        department: 'sad',
+        lastName: 'sad',
+        firstName: [],
+      }
+    ]
+    
+    tab.map(option => {
+      const employee = new Employee({...option});
+      employee.validate(err => {
+        expect(err.errors.firstName).to.exist;
+      });
+    })
+
+    after(() => {
+      mongoose.models = {};
+    });
+  });
+
+  it('should throw an error if no have lastName arg or its not string', () => {
+    const tab = [
+      {
+        department: 'sad',
+        firstName: 'sad',
+      },
+      {
+        department: 'sad',
+        firstName: 'sad',
+        lastName: {},
+      },
+      {
+        department: 'sad',
+        firstName: 'sad',
+        lastName: [],
+      }
+    ]
+    
+    tab.map(option => {
+      const employee = new Employee({...option});
+      employee.validate(err => {
+        expect(err.errors.lastName).to.exist;
+      });
+    })
+
+    after(() => {
+      mongoose.models = {};
+    });
+  });
+});
